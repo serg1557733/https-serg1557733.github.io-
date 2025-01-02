9a57c95322200c62ae7a5683351bdbf78e0a5a25
Serhii Borshch <72445490+serg1557733@users.noreply.github.com>
2025-01-02 20:24:31 +0100

    fixed test

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -70,19 +70,12 @@ const QuizPage = ({ onLogin }: LoginPageProps) => {
         <div className={`w-48`}>
           <img src={euro} alt="euro" />
         </div>
-        {answered && (
-          <>
-            {rightAnswer ? (
-              <div className="w-48">
-                <img src={mimi} alt="euro" />
-              </div>
-            ) : (
-              <div className="w-80 left-1 absolute animate-bounce">
-                <img src={cat} alt="cat" />
-              </div>
-            )}
-          </>
+        {answered && rightAnswer && (
+          <div className="w-80 left-1 absolute animate-bounce">
+            <img src={cat} alt="cat" />
+          </div>
         )}
+
         <h2 className="p-4 border rounded-3xl border-l-amber-300 border-solid font-bold bg-red-600">
           Ваш счет: {score} Euro
         </h2>
